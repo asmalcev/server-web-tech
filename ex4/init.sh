4e382885ec9f27da65d0c4e702553dd258fb6999
qzerrty <qzerrty@protonmail.com>
2024-09-17 21:03:20 +0300

    init repo +ex3 +ex4

---
+++ ex4/init.sh
@@ -0,0 +1,3 @@
+sudo apt install nodejs
+sudo apt install nginx
+sudo apt install net-tools
---
+++ ex4/simpleserver.js
@@ -0,0 +1,36 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET,POST,PUT,DELETE,OPTIONS'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'x-test,ngrok-skip-browser-warning,Content-Type,Accept,Access-Control-Allow-Headers'
+    );
+
+    let body = [];
+    req.on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+
+        const response = {
+            message: 'google.106893173198622652497',
+            ['x-test']: req.headers['x-test'],
+            ['x-body']: body,
+        };
+
+        res.end(JSON.stringify(response));
+    });
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+});
